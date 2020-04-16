cb3f660ec9fe4c175255b506a14395cb070edc2e
Kirill Vladimirov <kvladimirov.main@gmail.com>
2020-04-16 07:54:48 +0300

    commit previous work

---
+++ simcube/__init__.py
@@ -0,0 +1,4 @@
+from simcube import base
+from simcube import data
+from simcube import pipeline
+from simcube import visualization
---
+++ simcube/base.py
@@ -0,0 +1,5 @@
+def get_params_number(model):
+    """
+    
+    """
+    return sum(t.numel() for t in model.parameters())
